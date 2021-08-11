da22869fafe6db9e33720943d84d435797680dde
Gabriel Scota Fernandes <84752275+GabrielScotaEE@users.noreply.github.com>
2021-08-11 20:12:12 -0300

    Some code improoves added

--- getAndDraw.py
+++ getAndDraw.py
@@ -13,7 +13,7 @@ class getAndDrawBB:
         
 
     def get_bbox(self,results):
-        count = 0  
+        self.count = 0  
         self.detections_bb = []
         
         # Converting tensor to array
@@ -31,78 +31,72 @@ class getAndDrawBB:
 
                     self.detections_bb.append([x,y,x2,y2])
 
-                    count = count +1
+                    self.count = self.count +1
                 
 
-        return self.detections_bb, count
+        return self.detections_bb, self.count
 
        
-    def drawBBoxes(self, choose, frame, count, video, bb, tracker=False):
+    def drawBBoxes(self, choose,frame, video, bb, tracker=False):
+        self.choose = choose
+        self.frame = frame
+        self.bb_id = None
         if tracker is False:
             for rect in bb:
                 x, y, w, h = rect
-                cv.rectangle(frame, (x, y),(w, h), (0, 255, 5), 1)
+                cv.rectangle(self.frame, (x, y),(w, h), (0, 255, 5), 1)
+                self.showNumbers()
         else:
             raise Exception("This function don't have tracker, use drawBBoxes_with_ID() instead.")
-            
-        
 
-        # Showing the number os guns or people in each frame
-        if count > 9:
-            if choose ==1:
-                cv.rectangle(frame, (1268, 1),(1295, 22), (0, 0, 0), -1) 
-                cv.putText(frame,str(count), (1268,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,255,0),1)
-            else:
-                cv.rectangle(frame, (1176, 1),(1295, 22), (0, 0, 0), -1) 
-                cv.putText(frame,'Guns', (1088,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)
-                cv.putText(frame,str(count), (1172,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)
-        else:
-            if choose == 1:
-                cv.rectangle(frame, (1269, 1),(1295, 22), (0, 0, 0), -1) 
-                cv.putText(frame,str(count), (1275,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,255,0),1)
-            else:
-                cv.rectangle(frame, (1080, 1),(1199, 22), (0, 0, 0), -1) 
-                cv.putText(frame,'Guns', (1113,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)  
-                cv.putText(frame,str(count), (1180,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)  
         
+ 
         video.write(frame)    
         return frame
         
-    def drawBBoxes_with_ID(self, choose, frame, count, video, bb_id, tracker=True):
+    def drawBBoxes_with_ID(self, choose, frame, video, bb_id, tracker=True):
+        self.frame = frame
+        self.bb_id = bb_id
+        self.choose = choose
+
         if tracker is False:
             raise Exception("This function have tracker, use drawBBoxes() instead.")
-            
+        
         else:
-            for box_id in bb_id:
-                x, y, w, h,id = box_id
-                if choose ==1:
-                    cv.putText(frame,str(id), (x-2,y-10),cv.FONT_HERSHEY_COMPLEX,0.4,(0,255,0),1)
-                    cv.rectangle(frame, (x, y),(w, h), (0, 255, 0), 1)
-                else:
-                    cv.putText(frame,str(id), (x-2,y-10),cv.FONT_HERSHEY_COMPLEX,0.4,(0,0,255),1)
-                    cv.rectangle(frame, (x, y),(w, h), (0, 0, 255), 1)
-
-        # Showing the number os guns or people in each frame
-        if count > 9:
-            if choose ==1:
-                cv.rectangle(frame, (1268, 1),(1295, 22), (0, 0, 0), -1) 
-                cv.putText(frame,str(count), (1268,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,255,0),1)
-            else:
-                cv.rectangle(frame, (1176, 1),(1295, 22), (0, 0, 0), -1) 
-                cv.putText(frame,'Guns', (1088,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)
-                cv.putText(frame,str(count), (1172,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)
-        else:
-            if choose == 1:
-                cv.rectangle(frame, (1269, 1),(1295, 22), (0, 0, 0), -1) 
-                cv.putText(frame,str(count), (1275,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,255,0),1)
-            else:
-                cv.rectangle(frame, (1080, 1),(1199, 22), (0, 0, 0), -1) 
-                cv.putText(frame,'Guns', (1113,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)  
-                cv.putText(frame,str(count), (1180,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)  
-            
+            self.showNumbers()
+ 
         video.write(frame)
         return frame
+    
 
+    def showNumbers(self):
+        if self.bb_id is not None:
+            for box_id in self.bb_id:
+                    x, y, w, h,id = box_id
+                    if self.choose ==1:
+                        cv.putText(self.frame,str(id), (x-2,y-10),cv.FONT_HERSHEY_COMPLEX,0.4,(0,255,0),1)
+                        cv.rectangle(self.frame, (x, y),(w, h), (0, 255, 0), 1)
+                    else:
+                        cv.putText(self.frame,str(id), (x-2,y-10),cv.FONT_HERSHEY_COMPLEX,0.4,(0,0,255),1)
+                        cv.rectangle(self.frame, (x, y),(w, h), (0, 0, 255), 1)
+
+        if self.count > 9:
+            if self.choose ==1:
+                cv.rectangle(self.frame, (1268, 1),(1295, 22), (0, 0, 0), -1) 
+                cv.putText(self.frame,str(self.count), (1268,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,255,0),1)
+            else:
+                cv.rectangle(self.frame, (1176, 1),(1295, 22), (0, 0, 0), -1) 
+                cv.putText(self.frame,'Guns', (1088,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)
+                cv.putText(self.frame,str(self.count), (1172,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)
+        else:
+            if self.choose == 1:
+                cv.rectangle(self.frame, (1269, 1),(1295, 22), (0, 0, 0), -1) 
+                cv.putText(self.frame,str(self.count), (1275,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,255,0),1)
+            else:
+                cv.rectangle(self.frame, (1080, 1),(1199, 22), (0, 0, 0), -1) 
+                cv.putText(self.frame,'Guns', (1113,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)  
+                cv.putText(self.frame,str(self.count), (1180,20),cv.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),1)  
+        return
         
 
 
